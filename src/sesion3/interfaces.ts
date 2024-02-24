39ce5325d172f75783d1561e80c7904d73c27cb5
Roxana2829 <arenasroxana96@gmail.com>
2024-02-24 12:55:44 -0600

    interfaces - tipado estricto de objetos

---
+++ src/sesion3/interfaces.ts
@@ -0,0 +1,29 @@
+interface IdNme {
+    id: number;
+    name: string;
+    print: (text: string, age: number) => void
+}
+
+let idObj: IdNme = {
+    id: 1,
+    name:'Carlos',
+    print: (text, age) => console.log(text, age)
+}
+
+idObj.print('hola', 2)
+
+
+interface Optional {
+    id: number,
+    name: string,
+}
+
+//let optional: Optional ={
+ //   id: 2
+//}
+
+interface WeakTyep {
+    id?: number;
+    name?: string;
+}
+let weak: WeakTyep = {desc: 'rer'}
