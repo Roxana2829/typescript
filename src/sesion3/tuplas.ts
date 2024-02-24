b74bd920139af9c83fa73f6bd7039635930e9578
Roxana2829 <arenasroxana96@gmail.com>
2024-02-24 10:18:17 -0600

    Destructuracion

--- src/sesion3/tuplas.ts
+++ src/sesion3/tuplas.ts
@@ -7,11 +7,39 @@ tuple1 = ['test', true]
  */
 //tuple1 = [false, 'test']
 
-let a: boolean = false
+/**let a: boolean = false
 tuple1.forEach(value => {
     if (typeof value === 'boolean' ) {
         a = value
     }
 })
+*/
 
-console.log(tuple1)
+console.log(tuple1[0])
+console.log(tuple1[1])
+
+/**
+ * la estructurcion nos ahorra lineas de codigo
+ * evitando declarar en lines separadas cada variable
+ * 
+ */
+
+let [a, b] = tuple1
+
+console.log(a, b)
+
+
+let complexObj ={
+    aNum: 1,
+    bString: 'test',
+    cBool: true
+}
+
+
+/**
+ * la destructuracion no obliga a que se declaren todas las vriables en cada propiedad
+ * es decir podemos omitir valorews de un onjeto o arreglo
+ */
+let {aNum, bString, cBool} = complexObj
+
+console.log(aNum, bString, cBool)
