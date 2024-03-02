cc9ba90b832ce5962a5a07c1e3aabec461486887
Roxana2829 <arenasroxana96@gmail.com>
2024-03-02 11:22:33 -0600

    señalamiento

--- src/sesion4/clases.ts
+++ src/sesion4/clases.ts
@@ -1,9 +1,40 @@
 class SimpleClass {
+    private id: number
+    readonly name:string
+
+    constructor(name: string){
+        this.id = 1
+        this.name =name
+    }
+// public no es necesario señalarlo ya que esta por defecto
+    print(){
+        console.log('simpleClass.id= ' + this.id)
+    }
+/**
+ * 
+ * @param name las propiedades tipo readonly solo se pueden 
+ * asignar valores dentro del constructor
+ 
+    setName(name: string) {
+        this.name = name
+    }
+ */   
+}
+/**
+ * 
+ 
+let simpleClass = new SimpleClass('juan')
+
+simpleClass.print
+
+
+
+
     id: number | undefined
     print(){
         let id = 10
         console.log('simpleClass.id =' + this.id)
-        console.log(id)
+        console.log()
     }
 }
 
@@ -15,3 +46,4 @@ simpleClass.print()
 simpleClass2.id = 2024
 
 simpleClass2.print()
+*/
