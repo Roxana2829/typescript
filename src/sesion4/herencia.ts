251e898844b0b39ec82d32b3a41d12a7493948f3
Roxana2829 <arenasroxana96@gmail.com>
2024-03-02 12:49:07 -0600

    herencia-

---
+++ src/sesion4/herencia.ts
@@ -0,0 +1,56 @@
+interface Base {
+    id: number | string;
+}
+
+/**
+ * podemos disminuir el tipado mas no ampliarlo
+ */
+interface Base2 extends Base {
+    id: number;
+}
+
+/**
+ * se puede estender mas de una interfaz
+ * y no es necesario declarar las propiedades heredadas
+ */
+interface Derivada extends Base {
+    name: string;
+}
+
+class NameClass implements Derivada {
+     id: number;
+    name: string;
+
+    constructor(name: string){
+        this.id = 0
+        this.name =name
+    }
+
+    printName(){
+        console.log(this.name)
+    }
+}
+
+class ClassC implements Base2 {
+    id = 0;
+    ids =0
+}
+
+
+class AgeClass extends NameClass {
+    age: number = 0
+    ids = 0
+
+    constructor(name: string, age: number){
+        super(name)
+        this.age = age
+    }
+
+    printName() {
+        console.log(this.id)
+    }
+}
+
+let person: AgeClass = new AgeClass('Ana', 25) //{id: 10, name: 'ana', age: 24}
+
+person.printName()
